715c4637f1a64fa977b698d76e92f490aaab4316
fery kurniawan <ferykurniawan@ferys-MacBook-Pro.local>
2022-08-02 15:26:32 +0700

    add git submodule

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '../module/ui-story/components/Button';
+
+const Home = () => (
+  <div>
+    <p>Home</p>
+    <Button label='Button' primary />
+  </div>
+)
+
+export default Home
